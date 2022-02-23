96017bc356f8d59528db88f8cd2c039c891e0349
Aegis020404 <Aegis020404@gmail.com>
2022-02-23 19:51:44 +0300

    UI-BLL

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -14,23 +14,37 @@ const Message = (props) => {
     )
 }
 
+let dialogsData = [
+    {id: 1, name: 'Dimych'},
+    {id: 2, name: 'Andrey'},
+    {id: 3, name: 'Sveta'},
+    {id: 4, name: 'Sasha'},
+    {id: 5, name: 'Victor'},
+    {id: 6, name: 'Valera'},
+]
+let messagesData = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'How are you'},
+    {id: 3, message: 'Yo'},
+    {id: 4, message: 'Yo'},
+]
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name='Dimych' id='1'/>
-                <DialogItem name='Andrey' id='2'/>
-                <DialogItem name='Sveta' id='3'/>
-                <DialogItem name='Sasha' id='4'/>
-                <DialogItem name='Victor' id='5'/>
-                <DialogItem name='Dimych' id='6'/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
+                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
             </div>
             <div className={s.messages}>
-                <Message text='Hi'/>
-                <Message text='How are you?'/>
-                <Message text='Yo'/>
-                <Message text='Yo'/>
-                <Message text='Yo'/>
+                <Message text={messagesData[0].message} id={dialogsData[0].id}/>
+                <Message text={messagesData[1].message} id={dialogsData[1].id}/>
+                <Message text={messagesData[2].message} id={dialogsData[2].id}/>
+                <Message text={messagesData[3].message} id={dialogsData[3].id}/>
+
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,10 @@ import s from './MyPosts.module.css'
 import Post from './Post/Post'
 
 const MyPosts = () => {
+    let MyPostsData = [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It"s my first post', likesCount: 23},
+    ]
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
@@ -9,13 +13,13 @@ const MyPosts = () => {
                 <div>
                     <textarea name="" id="" cols="30" rows="10"></textarea>
                 </div>
-            <div>
-                <button>Add post</button>
-            </div>
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
             <div className={s.post}>
-                <Post message='Hi, how are you?' likesCount='0'/>
-                <Post message='It"s my first post' likesCount='23'/>
+                <Post message={MyPostsData[0].message} likesCount={MyPostsData[0].likesCount}/>
+                <Post message={MyPostsData[1].message} likesCount={MyPostsData[1].likesCount}/>
             </div>
         </div>
     )
