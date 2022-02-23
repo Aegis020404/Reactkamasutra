a1768879d6331a607ea31b8de0626d6b86aea12f
Aegis020404 <Aegis020404@gmail.com>
2022-02-23 16:19:28 +0300

    Route exat

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,34 @@
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to='/dialogs/1'>Dimych</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2'>Andrey</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/3>'>Sveta</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/4'>Valera</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/5'>Victor</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/6'>Valera</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
