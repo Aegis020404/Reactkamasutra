977ed0f84dea4f649cfa0e668118a41d19249a11
Aegis020404 <Aegis020404@gmail.com>
2022-03-04 19:19:56 +0300

    35. Уроки, Курс React JS - callback, subscribe, observer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import './index.css';
-import {rerenderEntireTree} from './render'
-import state from "./redux/state";
+import state, {subscribe} from "./redux/state";
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
+import React from "react";
+import {addPost, updateNewPostText} from "./redux/state";
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </BrowserRouter>, document.getElementById(`root`));
+}
+
 
 
 rerenderEntireTree(state)
+
+subscribe(rerenderEntireTree);
 
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,5 +1,4 @@
-import {rerenderEntireTree} from '../render'
-
+let rerenderEntireTree
 let state = {
     profilePage: {
         MyPostsData: [
@@ -30,7 +29,7 @@ let state = {
 window.state = state;
 
 
-export let addPost = (postMessage) => {
+export const addPost = () => {
     let newPost = {
         id: 5,
         message: state.profilePage.newPostText,
@@ -41,12 +40,17 @@ export let addPost = (postMessage) => {
     rerenderEntireTree(state)
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
 
     state.profilePage.newPostText = newText
     rerenderEntireTree(state)
 
 }
 
+export const subscribe = observer => {
+    rerenderEntireTree = observer; // наблюдатель .парттерн
+}
+
 export default state
 
+// strore - OOP
